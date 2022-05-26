9f101a0c3f60d13263e6c0b789f95047d3398b1b
rtavares <ricardotavaresit@gmail.com>
2022-05-26 18:46:03 +0100

    create  Component One

---
+++ src/components/TestComponentTwo.vue
@@ -0,0 +1,19 @@
+<template>
+  <p>Test Component Two - {{ msg }} </p>
+</template>
+
+<script>
+  export default {
+    data(){
+        return{
+          msg: "Course Vue JS"
+        }
+    }
+  }
+</script>
+
+<style scoped>
+  p{
+    color: blue
+  }
+</style>
