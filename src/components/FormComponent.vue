98dd12043b8f94f3583f508dc0bd340b76777c93
rtavares <ricardotavaresit@gmail.com>
2022-05-26 18:52:50 +0100

    create  Component Form

---
+++ src/components/FormComponent.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    {{ msg }}
+  </div>
+
+</template>
+
+
+<script>
+export default{
+  data(){
+    return {
+      msg: "I'm a green Form"
+    }
+  }
+}
+
+</script>
+
+
+<style scoped>
+  div{
+    background: green;
+  }
+</style>
