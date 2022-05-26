df6eb2811c75124a70e6f8f1de0a853231351c26
rtavares <ricardotavaresit@gmail.com>
2022-05-26 20:20:57 +0100

    create tasks component

---
+++ src/components/TaskComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <table>
+      <tr>
+        <th>Name</th>
+        <th>Status</th>
+      </tr>
+      <tr v-bind:key="index" v-for="(task, index) in tasks" >
+        <td> {{ task.name}}</td>
+        <td> {{ task.completed}}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    data(){
+      return{
+            tasks: [
+                { name: "Task 1", completed: true },
+                { name: "Task 2", completed: false },
+                { name: "Task 3", completed: true },
+                { name: "Task 4", completed: false}
+              ]
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
